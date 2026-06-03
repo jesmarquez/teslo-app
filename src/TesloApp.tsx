011be3844bbfb026c3c163720d07a0ec3cb838e1
Dell_JESMQZ <jesmqz@gmail.com>
2026-06-03 10:49:03 -0400

    Create pages

---
+++ src/TesloApp.tsx
@@ -0,0 +1,6 @@
+
+export const TesloApp = () => {
+  return (
+    <div>TesloApp</div>
+  )
+}
---
+++ src/admin/pages/dashboard/DashBoardPage.tsx
@@ -0,0 +1,6 @@
+
+export const DashBoardPage = () => {
+  return (
+    <div>DashBoardPage</div>
+  )
+}
