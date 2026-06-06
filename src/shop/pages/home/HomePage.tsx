77ee43d4db4c0ba0d698265621f1653defce5db7
Dell_JESMQZ <jesmqz@gmail.com>
2026-06-06 14:34:42 -0400

    Create pagination and products

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,15 +1,21 @@
-import { Button } from "../../../components/ui/button"
+import { CustomJumbotron } from "../../components/CustomJumbotron"
+import { CustomPagination } from '../../../components/custom/CustomPagination';
+import { ProductsGrid } from "../../components/ProductsGrid";
+import { products } from "../../../mocks/products.mock";
 
 export const HomePage = () => {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      {/* <h1>Hola mundo</h1>
       <h1 className="font-montserrat">Lets to work</h1>
       <h1 className="font-montserrat font-thin">Lets to work</h1>
       <h1 className="font-montserrat font-normal">Lets to work</h1>
-      <h1 className="font-montserrat font-bold">Lets to work</h1>
+      <h1 className="font-montserrat font-bold ">Lets to work</h1>
 
-      <Button>Hola mundo</Button>
+      <Button>Hola mundo</Button> */}
+      <ProductsGrid products= { products }/>
+      <CustomJumbotron title={"Todos los productos"} subTitle={""} />
+      <CustomPagination totalPages={ 7 }/>
     </div>
   )
 }
