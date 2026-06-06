b4e833a5215f2732757218d2ecdd049aed77a7d1
Dell_JESMQZ <jesmqz@gmail.com>
2026-06-06 10:15:33 -0400

    Install tailwind shadCN UI google fonts

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,6 +1,15 @@
+import { Button } from "../../../components/ui/button"
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Hola mundo</h1>
+      <h1 className="font-montserrat">Lets to work</h1>
+      <h1 className="font-montserrat font-thin">Lets to work</h1>
+      <h1 className="font-montserrat font-normal">Lets to work</h1>
+      <h1 className="font-montserrat font-bold">Lets to work</h1>
+
+      <Button>Hola mundo</Button>
+    </div>
   )
 }
