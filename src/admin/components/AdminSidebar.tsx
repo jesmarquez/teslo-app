daefc7adc06d0acdc6c2b124b2ebeadf0aac5038
Dell_JESMQZ <jesmqz@gmail.com>
2026-06-11 21:02:37 -0400

    Navigation admin sidebar

--- src/admin/components/AdminSidebar.tsx
+++ src/admin/components/AdminSidebar.tsx
@@ -12,6 +12,7 @@ import {
   ChevronRight
 } from 'lucide-react';
 import { CustomLogo } from '../../components/custom/CustomLogo';
+import { Link, useLocation } from 'react-router';
 
 interface SidebarProps {
   isCollapsed: boolean;
@@ -19,9 +20,14 @@ interface SidebarProps {
 }
 
 export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) => {
+
+  const { pathname } = useLocation();
+
+  console.log( { pathname });
+
   const menuItems = [
-    { icon: Home, label: 'Dashboard', active: true },
-    { icon: BarChart3, label: 'Analytics' },
+    { icon: Home, label: 'Dashboard', to: '/admin'},
+    { icon: BarChart3, label: 'Products', to: '/admin/products' },
     { icon: Users, label: 'Users' },
     { icon: ShoppingCart, label: 'Orders' },
     { icon: FileText, label: 'Reports' },
@@ -30,6 +36,9 @@ export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) 
     { icon: HelpCircle, label: 'Help' },
   ];
 
+  const isActiveRoute = ( to: string ) => {
+    return pathname === to;
+  }
   return (
     <div className={`bg-white border-r border-gray-200 transition-all duration-300 ease-in-out ${
       isCollapsed ? 'w-18' : 'w-64'
@@ -52,10 +61,10 @@ export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) 
             const Icon = item.icon;
             return (
               <li key={index}>
-                <a
-                  href="#"
+                <Link
+                  to ={ item.to || '/admin' }
                   className={`flex items-center space-x-3 px-3 py-2 rounded-lg transition-all duration-200 group ${
-                    item.active
+                    isActiveRoute(item.to || '/xxxx')
                       ? 'bg-blue-50 text-blue-600 border-r-2 border-blue-600'
                       : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
                   }`}
@@ -64,7 +73,7 @@ export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) 
                   {!isCollapsed && (
                     <span className="font-medium">{item.label}</span>
                   )}
-                </a>
+                </Link>
               </li>
             );
           })}
